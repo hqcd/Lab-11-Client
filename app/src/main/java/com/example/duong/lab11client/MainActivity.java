7608b31eb7a5441ec06c5449a31eb3683084e537
Quinten <duongquinten@gmail.com>
2018-11-16 12:31:50 -0500

    Finished

--- app/src/main/java/com/example/duong/lab11client/MainActivity.java
+++ app/src/main/java/com/example/duong/lab11client/MainActivity.java
@@ -1,18 +1,18 @@
 package com.example.duong.lab11client;
 
 import android.content.ContentResolver;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.database.Cursor;
 
 public class MainActivity extends AppCompatActivity {
 
     Button retrieveBtn;
     TextView studentList;
-    String[] myColumnProject = new String[] {
-        "content://students.com.duong/STUDENTS",
-    };
 
     ContentResolver contentResolver;
 
@@ -21,6 +21,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        retrieveBtn = (Button)findViewById(R.id.retrieveButton);
+        studentList = (TextView)findViewById(R.id.studentList);
 
+        contentResolver = getContentResolver();
+
+        retrieveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                retrieve();
+            }
+        });
+    }
+
+    public void retrieve()
+    {
+        Cursor cursor = contentResolver.query(Uri.parse("content://students.com.duong/STUDENTS"), null, null, null, null, null);
+        StringBuilder list = new StringBuilder("");
+        if(cursor!= null && cursor.getCount() > 0)
+        {
+            while(cursor.moveToNext())
+            {
+                list.append("Stduent: " + cursor.getString(1) + " , " + "Grade: " + cursor.getInt(2) + "\n");
+            }
+        }
+        cursor.close();
+
+        studentList.setText(list.toString());
     }
 }
